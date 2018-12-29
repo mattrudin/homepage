6efcab2dcbbbf70e14052e61d432e57d320076f2
Matthias Rudin <matthias.rudin@live.com>
2018-12-29 17:51:48 +0100

    Updated CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import './layout.css'
 
@@ -14,25 +15,36 @@ const LAYOUT_QUERY = graphql`
   }
   `
 
+const LayoutWrapper = styled.div`
+  font-family: sans-serif;
+  font-size: 80%;
+  color: #373737;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  h1 {
+    font-family: Segoe UI;
+    font-weight: 700;
+    font-size: 2em;
+  }
+  @media only screen and (min-width: 600px) {
+    font-size: 120%;
+  }
+`
+
 const Layout = ({ children }) => (
   <StaticQuery
     query={LAYOUT_QUERY}
     render={data => (
       <>
-        <div
-          style={{
-            display: `flex`,
-            flexDirection: `column`,
-            justifyContent: `center`,
-            alignItems: `center`,
-            height: `100vh`,
-          }}
-        >
+        <LayoutWrapper>
           {children}
           <footer>
-            © MattRudin, MADE IN BASLE / LUCERNE
+            <p>© MattRudin, MADE IN BASLE / LUCERNE</p>
           </footer>
-        </div>
+        </LayoutWrapper>
       </>
     )}
   />
