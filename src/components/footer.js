fc90a28a0cb84d598b1553388d07d6c263cf4185
Matthias Rudin <matthias.rudin@live.com>
2018-12-29 18:52:32 +0100

    Fixed Github icon bug

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 const Footer = () => (
     <footer>
